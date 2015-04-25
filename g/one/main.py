141a2b963ae25ac53f1f31bd0aef5cc1a3f2e3d4
Matthew Bekkema <mbekkema97@gmail.com>
2015-04-25 20:43:19 +1000

    Allow resizable window + math

--- g/one/main.py
+++ g/one/main.py
@@ -5,7 +5,7 @@ from pyglet.window import key
 
 class GameWindow(pyglet.window.Window):
     def __init__(self):
-        pyglet.window.Window.__init__(self)
+        pyglet.window.Window.__init__(self,width=854,height=480,resizable=True)
         self.register_event_type('player1')
 
     def on_draw(self):
@@ -19,6 +19,18 @@ class GameWindow(pyglet.window.Window):
     def on_key_release(self, symbol, modifiers):
         if symbol in [key.LEFT,key.RIGHT,key.UP,key.DOWN]:
             self.dispatch_event('player1', symbol, False)
+    def on_resize(self, width, height):
+        adjwidth = (height * 16) // 9
+        adjheight = (width * 9) // 16
+        if adjwidth < width:
+            pyglet.gl.glViewport((width-adjwidth)//2, 0, adjwidth, height)
+        else:
+            pyglet.gl.glViewport(0, (height-adjheight)//2, width, adjheight)
+
+        pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
+        pyglet.gl.glLoadIdentity()
+        pyglet.gl.glOrtho(0, 854, 0, 480, -1, 1)
+        pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
 
 def main():
     Resources.init()
