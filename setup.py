25d3f3c9dfc51c4c0f52c7261980afc4955ad55a
Matthew Bekkema <mbekkema97@gmail.com>
2015-06-07 14:03:23 +1000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="G - One",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'gui_scripts': [
+            'G-One = g.one.main:main',
+        ]
+    },
+    package_data={
+        'g.one.resources': ['*.png']
+    }
+)
